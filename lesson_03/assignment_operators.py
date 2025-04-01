1b22f335b2065525f6ce3535d4480d0e06de71c2
AlexKomanov <akomanov88@gmail.com>
2025-04-01 22:03:46 +0300

    assignment operators

---
+++ lesson_03/assignment_operators.py
@@ -0,0 +1,26 @@
+a = 5
+print(a)
+
+# a = a + 1
+# a++  -> not valid in Python
+a += 1
+print(a)
+
+a -= 2
+print(a)
+
+a *= 50
+print(a)
+
+a //= 3
+print(a)
+
+a /= 5
+print(a)
+
+a **= 3
+print(a)
+
+a %= 10
+print(a)
+
